4705d18f4ebc8ba62089d5a296a86a5d418d750f
Ceren_Eyg <cerenka@gmail.com>
2023-03-07 10:21:27 -0600

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/ceren/Documents/GitHub/cavsiopy/src/cavsiopy/'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
