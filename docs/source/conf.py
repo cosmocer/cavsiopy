7919be8ccf583f288b7bbba03be4d09c59281f61
Ceren_Eyg <cerenka@gmail.com>
2023-03-07 17:30:56 -0600

    refreshed docs-erased the ones which are not working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import os
-import sys
-sys.path.insert(0, os.path.abspath('/home/ceren/Documents/GitHub/cavsiopy/cavsiopy'))
+
 
 # -- Project information -----------------------------------------------------
 
@@ -32,7 +30,7 @@ release = '0.9.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,8 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#
-html_theme = 'sphinxdoc'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
